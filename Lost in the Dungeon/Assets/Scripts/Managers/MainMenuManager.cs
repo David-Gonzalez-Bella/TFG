559c528c5c7237ff9215eebfa108cf592560d5e1
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2021-05-10 23:40:31 +0200

    Volume bars
    Volume bars in pause screen and in the main menu

--- Lost in the Dungeon/Assets/Scripts/Managers/MainMenuManager.cs
+++ Lost in the Dungeon/Assets/Scripts/Managers/MainMenuManager.cs
@@ -7,10 +7,24 @@ public class MainMenuManager : MonoBehaviour
 {
     public Button[] buttons;
 
+    public Slider musicVolumeBar;
+    public Slider sfxVolumeBar;
+
     private void Start()
     {
+        AudioManager am = AudioManager.sharedInstance.GetComponent<AudioManager>();
+
         for (int i = 0; i < buttons.Length; i++)
             buttons[i].onClick.AddListener(AudioManager.sharedInstance.PlaySelectSound);
+
+        am.musicAudioMixer.GetFloat("MusicVolume", out float musicVolume);
+        am.sfxAudioMixer.GetFloat("SFX_Volume", out float sfxVolume);
+
+        musicVolumeBar.value = (Mathf.Pow(10, (musicVolume / 20)));
+        sfxVolumeBar.value = (Mathf.Pow(10, (sfxVolume / 20)));
+
+        musicVolumeBar.onValueChanged.AddListener(am.SetMusicVolume);
+        sfxVolumeBar.onValueChanged.AddListener(am.SetSFX_Volume);
     }
 
     public void GoToChooseLength() => Transitions.sharedInstance.TransitionToChooseLength();
--- Lost in the Dungeon/Assets/Scripts/Menus/PauseMenu.cs
+++ Lost in the Dungeon/Assets/Scripts/Menus/PauseMenu.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public Slider musicVolumeBar;
+    public Slider sfxVolumeBar;
+
+    private void Start()
+    {
+        AudioManager am = AudioManager.sharedInstance.GetComponent<AudioManager>();
+
+        am.musicAudioMixer.GetFloat("MusicVolume", out float musicVolume);
+        am.sfxAudioMixer.GetFloat("SFX_Volume", out float sfxVolume);
+
+        musicVolumeBar.value = (Mathf.Pow(10, (musicVolume / 20)));
+        sfxVolumeBar.value = (Mathf.Pow(10, (sfxVolume / 20)));
+
+        musicVolumeBar.onValueChanged.AddListener(am.SetMusicVolume);
+        sfxVolumeBar.onValueChanged.AddListener(am.SetSFX_Volume);
+    }
+
     public void TimeScaleStop() => Time.timeScale = 0.0f;
     public void TimeScaleResume() => Time.timeScale = 1.0f;
 }
